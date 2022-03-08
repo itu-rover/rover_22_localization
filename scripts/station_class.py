43a7eb16f13dee90121c9edfaa83e88583f7118d
Ata Parlar <ataparlars5@gmail.com>
2022-03-08 20:37:59 +0300

    added RoverStation class, not tried yet

--- scripts/station_class.py
+++ scripts/station_class.py
@@ -11,7 +11,10 @@ class ReferenceStation():
     
     def __init__(self):
         """ 
-            Updates the mean of lat, long, alt
+            Initialize the node, subscribers and publishers.
+            Set the initial values of mean coordinates and 
+            coordinate's number.
+            Set the current time.
         """
         rospy.init_node("gnss_publisher")
 
@@ -26,12 +29,12 @@ class ReferenceStation():
         self.current_time = rospy.Time.now()
 
         # Publisher part
-        self.ref_pub = rospy.Publisher("/gnss/ref/fix", NavSatFix, queue_size=10)
-        self.ref_err_pub = rospy.Publisher("/gnss/ref/error", Float64MultiArray)
+        self.ref_coord_pub = rospy.Publisher("/gnss/ref/fix_mean", NavSatFix)
+        self.ref_coord_err_pub = rospy.Publisher("/gnss/ref/error", Float64MultiArray)
 
         # Subscriber part
-        rospy.Subscriber("/fix", NavSatFix, self.mean_updater)
-        rospy.Subscriber("/fix", NavSatFix, self.difference_calculator)
+        rospy.Subscriber("/gnss/ref/fix", NavSatFix, self.mean_updater)
+        rospy.Subscriber("/gnss/ref/fix", NavSatFix, self.difference_calculator)
 
     
     def mean_updater(self, data):
@@ -54,7 +57,6 @@ class ReferenceStation():
         # Create NavSatFix message
         self.ref_nav = NavSatFix()
         self.ref_nav.header.stamp = self.current_time
-        self.ref_nav.header.frame_id = "base_link"
         self.ref_nav.latitude = self.mean_coords[0]
         self.ref_nav.longitude = self.mean_coords[1]
         self.ref_nav.altitude = self.mean_coords[2]
@@ -65,7 +67,7 @@ class ReferenceStation():
         ]
         
         # Publish the message
-        self.ref_pub.publish(self.nav)
+        self.ref_coord_pub.publish(self.nav)
 
 
 
@@ -87,9 +89,12 @@ class ReferenceStation():
             self.difference_list.append(self.mean_coords[counter] - coord)
             counter+=1
         
-        self.ref_dif = Float64MultiArray()
-        self.ref_dif.data = self.difference_list
-        self.ref_err_pub.publish(self.ref_dif)
+        self.ref_coord_dif = Float64MultiArray()
+        self.ref_coord_dif.data = self.difference_list
+        self.ref_coord_err_pub.publish(self.ref_coord_dif)
+
+        # Return to use in RoverStation class
+        return self.ref_coord_dif
 
 
 
@@ -99,23 +104,45 @@ class RoverStation(self):
         """ 
             
         """
-        pass
+        self.reference = ReferenceStation()
+
+        rospy.init_node("coordinate_corrector")
+        self.current_time = rospy.Time.now()
+
+        self.rover_coord_pub = rospy.Publisher("/gnss/rover/fix", NavSatFix, queue_size=10)
+
+        rospy.Subscriber("/gnss/rover/fix", Float64MultiArray, self.coordinate_corrector)
+
     
 
-    def coordinate_corrector(self):
+    def coordinate_corrector(self, data):
         """ 
             Makes the coordinate corrections comes from
             the reference stations.
             
-            returns corrected lat, long, alt
+            Publishes corrected lat, long, alt
         """
-        pass
+
+        # Take the correction values from Reference Station class
+        # [lat, long, alt]
+        correction_values = self.reference.difference_calculator()
+        self.rover_nav = NavSatFix()
+        self.rover_nav.header.stamp = self.current_time
+        self.rover_nav.header.frame_id = "base_link"
+        self.rover_nav.latitude = data.latitude - correction_values[0]
+        self.rover_nav.longitude = data.longitude - correction_values[1]
+        self.rover_nav.altitude = data.altitude - correction_values[2]
+        self.rover_nav.position_covariance = [
+            0, 0, 0,
+            0, 0, 0,
+            0, 0, 0
+        ]
 
 
 
 if __name__ == "__main__":
     try:
-        pass
+        ReferenceStation()
+        RoverStation()
     except rospy.ROSInterruptException:
         pass
-
